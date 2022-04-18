d53c782ff8756a9295289ca7bf916e7505e32e69
Jarrod Hoover <hoov85@gmail.com>
2022-04-18 16:51:38 -0400

    added image preview

--- src/app/components/posts/post-create/post-create.component.ts
+++ src/app/components/posts/post-create/post-create.component.ts
@@ -13,6 +13,7 @@ export class PostCreateComponent implements OnInit {
   post: Post;
   isLoading: boolean = false;
   form: FormGroup;
+  imagePreview: string;
   private mode: string = 'create';
   private postId: string;
 
@@ -21,7 +22,8 @@ export class PostCreateComponent implements OnInit {
   ngOnInit(): void {
     this.form = new FormGroup({
       title: new FormControl(null, { validators: [Validators.required, Validators.minLength(3)] }),
-      content: new FormControl(null, { validators: [Validators.required] })
+      content: new FormControl(null, { validators: [Validators.required] }),
+      image: new FormControl(null, { validators: [Validators.required] })
     });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('postId')) {
@@ -31,13 +33,24 @@ export class PostCreateComponent implements OnInit {
         this.postsService.getPost(this.postId).subscribe((postData) => {
           this.isLoading = false;
           this.post = { id: postData._id, title: postData.title, content: postData.content };
-          this.form.setValue({ title: this.post.title, content: this.post.content });
+          this.form.setValue({ title: this.post.title, content: this.post.content, image: null });
         });
       } else {
         this.mode = 'create';
         this.postId = null;
       }
     });
+  }
+
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({ image: file });
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result as string;
+    };
+    reader.readAsDataURL(file);
   }
 
   onSavePost(): void {

5a0ca25d1c059f2a52e8bfc482f408768a02cddf
Jarrod Hoover <hoov85@gmail.com>
2022-04-29 09:14:02 -0400

    added multer storage configuration

--- api/routes/posts.ts
+++ api/routes/posts.ts
@@ -1,9 +1,33 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { PostModel } from '../models/post';
+import multer, { StorageEngine } from 'multer';
 
 export const postsRouter = express.Router();
 
-postsRouter.post('', (req: Request, res: Response, next: NextFunction) => {
+const MIME_TYPE_MAP: Record<string, string> = {
+  'image/png': 'png',
+  'image/jpeg': 'jpeg',
+  'image/jpg': 'jpg'
+};
+
+// Multer disk storage configuration
+const diskStorage: StorageEngine = multer.diskStorage({
+  destination: (req, file, cb): void => {
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error('API:Invalid mime type');
+    if (isValid) {
+      error = null;
+    }
+    cb(error, 'api/images');
+  },
+  filename: (req, file, cb): void => {
+    const name = file.originalname.toLocaleLowerCase().split(' ').join('-');
+    const ext = MIME_TYPE_MAP[file.mimetype];
+    cb(null, `${name}-${Date.now()}.${ext}`);
+  }
+});
+
+postsRouter.post('', multer({ storage: diskStorage }).single('image'), (req: Request, res: Response, next: NextFunction) => {
   const post = new PostModel({
     title: req.body.title,
     content: req.body.content

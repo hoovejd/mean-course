669d4620b7f5d1037da1d830e349049e32f9264b
Jarrod Hoover <hoov85@gmail.com>
2022-07-22 13:31:46 -0400

    refactored out post module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,22 @@
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { SignupComponent } from './components/auth/signup/signup.component';
+import { ErrorComponent } from './components/error/error.component';
 import { HeaderComponent } from './components/header/header.component';
-import { PostCreateComponent } from './components/posts/post-create/post-create.component';
-import { PostListComponent } from './components/posts/post-list/post-list.component';
+import { PostsModule } from './components/posts/posts.module';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
 import { ErrorInterceptor } from './interceptors/error.interceptor';
-import { ErrorComponent } from './components/error/error.component';
-import { AngularMaterialModule } from './angular-material.module';
 
 @NgModule({
-  declarations: [AppComponent, PostCreateComponent, HeaderComponent, PostListComponent, LoginComponent, SignupComponent, ErrorComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule, BrowserAnimationsModule, HttpClientModule, AngularMaterialModule],
+  declarations: [AppComponent, HeaderComponent, LoginComponent, SignupComponent, ErrorComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, BrowserAnimationsModule, HttpClientModule, AngularMaterialModule, PostsModule],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
